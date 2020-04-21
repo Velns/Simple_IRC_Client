debd48572195f80bbfb4d5cd9db77b5f9ff8986a
Денис Чорнолоз <den.denis999@gmail.com>
2020-04-22 00:45:00 +0300

    Simple IRC upload

---
+++ IrcClient/IrcClient.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Net;
+
+namespace IrcClient
+{
+    class ircClient
+    {
+        private string userName;
+        private string channel;
+        private TcpClient tcpClient;
+        private StreamReader inputStream;
+        private StreamWriter outpurStream;
+        private string address = "";
+        private int port = 6667;
+
+        public ircClient(string ip, string userName)
+        {
+            this.userName = userName;
+            tcpClient = new TcpClient(ip, port);
+            address = ((IPEndPoint)tcpClient.Client.LocalEndPoint).Address.ToString();
+            inputStream = new StreamReader(tcpClient.GetStream());
+            outpurStream = new StreamWriter(tcpClient.GetStream());
+            outpurStream.WriteLine("NICK " + userName);
+            outpurStream.WriteLine("USER " + userName + " 8 * " + userName);
+            outpurStream.Flush();
+
+        }
+        public void joinRoom(string channel)
+        {
+            this.channel = channel;
+            outpurStream.WriteLine("JOIN #" + channel);
+            outpurStream.Flush();
+
+        }
+        public void sendIrcMessage(string message)
+        {
+            outpurStream.WriteLine(message);
+            outpurStream.Flush();
+
+        }
+        public void sendChatMessage(string message)
+        {
+            sendIrcMessage(":" + userName + " PRIVMSG #" + channel + " :" + message);
+                    }
+        
+        public string readMessage()
+        {
+            string message = inputStream.ReadLine();
+            return message;
+        }
+        
+    }
+}
---
+++ IrcClient/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace IrcClient
+{
+    class Program
+    {
+        static string server;
+        static string username;
+        static string port;
+        static string channel;
+        static int pongs = 0;
+        static int count = 0;
+
+        static void Main(string[] args)
+        {
+            SetParameters();
+            ircClient irc = new ircClient(server, username);
+            irc.joinRoom(channel);
+            try
+            {
+                while (true)
+                {
+                    string message = irc.readMessage();
+                    if (message.Contains("/NAMES list") && message != null)
+                    {
+                        Console.Title = "Connected to: " + channel + ". Messages: " + count + ". Pongs: " +  pongs;
+                        break;
+                    }
+                }
+                Thread t = new Thread(() => readChat(irc));
+                t.Start();
+                Thread send = new Thread(() => sendMessage(irc));
+                send.Start();
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine("Error: " + x);
+            }
+            while(true)
+            {
+                Thread.Sleep(1000);
+            }
+
+        }
+        static void readChat(ircClient irc)
+        {
+            
+            while (true)
+            {
+                try
+                {
+                    string message = irc.readMessage();
+                    if (message.Contains("PING"))
+                    {
+                        irc.sendIrcMessage(message.Replace("PING", "PONG"));
+                        Console.WriteLine("PONG message sent");
+                        pongs++;
+                        Console.Title = "Connected to: " + channel + ". Messages: " + count + ". Pongs: " + pongs;
+                    }
+                    else if (message != null)
+                    {
+                        var offset = message.IndexOf(':');
+                        var result = message.IndexOf(':', offset + 1);
+                        string userName = message.Substring(1, message.IndexOf("!") - 1);
+                        message = message.Substring(result + 1);
+
+                        Console.Write(DateTime.Now.ToString("HH:mm tt("));
+                        Console.BackgroundColor = ConsoleColor.DarkRed;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.Write(userName);
+                        Console.ResetColor();
+                        Console.WriteLine(") " + message);
+                        count++;
+                        Console.Title = "Connected to: " + channel + ". Messages: " + count + ". Pongs: " + pongs;
+                    }
+                }
+                catch
+                {
+
+                }
+            }             
+        }
+        static void sendMessage(ircClient irc)
+        {
+            while(true)
+            {
+                string sendingMessage = Console.ReadLine();
+                irc.sendChatMessage(sendingMessage);
+            }
+        }
+        static void SetParameters()
+        {
+            Console.WriteLine("Enter IRC server address:");
+            server = "irc.freenode.net";        //Console.ReadLine();
+            
+            Console.WriteLine("Enter IRC server port:");
+            port = "6667";                      //Console.ReadLine();
+            
+
+            Console.WriteLine("Enter username:");
+            username = "tester_bot_1";          // Console.ReadLine();
+            
+            Console.WriteLine("Enter channel:");
+            channel = "testing_bot";            // Console.ReadLine();
+            
+            
+        }
+    }
+}
